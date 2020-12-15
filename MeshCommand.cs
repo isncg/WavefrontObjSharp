7cde5d9b396d7d6533543077e590df8a8d20df4a
yishenhan <yishenhan@tencent.com>
2020-12-15 12:50:15 +0800

    command o

--- MeshCommand.cs
+++ MeshCommand.cs
@@ -4,53 +4,56 @@ using System.Text;
 
 namespace WavefrontObjSharp
 {
-    public class MeshVertexCommand : IObjCommand
-    {
-        public Vertex.Component Component { get; private set; }
-        public MeshVertexCommand(Vertex.Component component)
-        {
-            Component = component;
-        }
+	public class MeshVertexCommand : IObjCommand
+	{
+		public Vertex.Component Component { get; private set; }
+		public MeshVertexCommand(Vertex.Component component)
+		{
+			Component = component;
+		}
 
-        public void Execute(List<string> param, ObjCommandContext context)
-        {
-            var vec = Vector.Parse(param.ToArray());
-            context.currentMesh.data[(int)Component].Add(vec);
-        }
+		public void Execute(List<string> param, ObjCommandContext context)
+		{
+			var vec = Vector.Parse(param.ToArray());
+			context.currentMesh.data[(int)Component].Add(vec);
+		}
+	}
 
-        public static MeshVertexCommand Create(string name)
-        {
-            if (name == "v")
-                return new MeshVertexCommand(Vertex.Component.Position);
-            else if (name == "vt")
-                return new MeshVertexCommand(Vertex.Component.UV);
-            else if (name == "vn")
-                return new MeshVertexCommand(Vertex.Component.Noraml);
-            return null;
-        }
-    }
+	public class MeshFaceCommand : IObjCommand
+	{
+		public void Execute(List<string> param, ObjCommandContext context)
+		{
+			var vertices = param.ConvertAll<Vertex>(str => Vertex.Parse(str));
+			if (vertices.Count > 0)
+			{
+				if (context.currentMesh.vertexVaiidFlag == 0)
+					context.currentMesh.vertexVaiidFlag = vertices[0] != null ? vertices[0].GetValidFlag() : 0;
+				if (context.currentMesh.vertexVaiidFlag == 0)
+					return;
+				context.currentMesh.AddFace(vertices);
+			}
+		}
+	}
 
-    public class MeshFaceCommand : IObjCommand
-    {
-        public void Execute(List<string> param, ObjCommandContext context)
-        {
-            var vertices = param.ConvertAll<Vertex>(str => Vertex.Parse(str));
-            if (vertices.Count > 0)
-            {
-                if (context.currentMesh.vertexVaiidFlag == 0)
-                    context.currentMesh.vertexVaiidFlag = vertices[0] != null ? vertices[0].GetValidFlag() : 0;
-                if (context.currentMesh.vertexVaiidFlag == 0)
-                    return;
-                context.currentMesh.AddFace(vertices);
-            }
-        }
-    }
+	public class MeshDumpCommand : IObjCommand
+	{
+		public void Execute(List<string> param, ObjCommandContext context)
+		{
+			Console.WriteLine(Utils.Dump(context.currentMesh, "    "));
+		}
+	}
 
-    public class MeshDumpCommand : IObjCommand
-    {
-        public void Execute(List<string> param, ObjCommandContext context)
-        {
-            Console.WriteLine(Utils.Dump(context.currentMesh, "    "));
-        }
-    }
+
+	public class MeshCreateCommand : IObjCommand
+	{
+		public void Execute(List<string> param, ObjCommandContext context)
+		{
+			string name = param.Count > 0 ? param[0] : string.Empty;
+			if (string.IsNullOrEmpty(name))
+				name = "mesh";
+			var mesh = context.CreateNewMesh(name);
+			context.meshDict.Add(mesh.name, mesh);
+			context.currentMesh = mesh;
+		}
+	}
 }
--- ObjCommand.cs
+++ ObjCommand.cs
@@ -4,51 +4,66 @@ using System.Text;
 
 namespace WavefrontObjSharp
 {
-    public interface IObjCommand
-    {
-        void Execute(List<string> param, ObjCommandContext context);
-    }
+	public interface IObjCommand
+	{
+		void Execute(List<string> param, ObjCommandContext context);
+	}
 
 
-    public class ObjCommandContext
-    {
-        public Mesh currentMesh = new Mesh();
+	public class ObjCommandContext
+	{
+		public Mesh currentMesh = new Mesh();
+		public Dictionary<string, Mesh> meshDict = new Dictionary<string, Mesh>();
 
-        Dictionary<string, IObjCommand> commands = new Dictionary<string, IObjCommand>();
+		public Mesh CreateNewMesh(string name)
+		{
+			int retryCount = 1;
+			string retryName = name;
+			while (meshDict.ContainsKey(retryName))
+			{
+				retryName = string.Format("{0}_{1}", name, retryCount);
+				retryCount++;
+			}
+			name = retryName;
+			return new Mesh { name = retryName };
+		}
 
-        public void RegisterCommands()
-        {
-            commands.Add("v", new MeshVertexCommand(Vertex.Component.Position));
-            commands.Add("vn", new MeshVertexCommand(Vertex.Component.Noraml));
-            commands.Add("vt", new MeshVertexCommand(Vertex.Component.UV));
-            commands.Add("f", new MeshFaceCommand());
-            commands.Add("dump", new MeshDumpCommand());
-        }
+		Dictionary<string, IObjCommand> commands = new Dictionary<string, IObjCommand>();
 
-        public void NextLine(string line)
-        {
-            if (string.IsNullOrWhiteSpace(line))
-                return;
+		public void RegisterCommands()
+		{
+			commands.Add("v", new MeshVertexCommand(Vertex.Component.Position));
+			commands.Add("vn", new MeshVertexCommand(Vertex.Component.Noraml));
+			commands.Add("vt", new MeshVertexCommand(Vertex.Component.UV));
+			commands.Add("f", new MeshFaceCommand());
+			commands.Add("o", new MeshCreateCommand());
+			commands.Add("dump", new MeshDumpCommand());
+		}
 
-            var split = line.Split(" ");
-            List<string> tokens = new List<string>();
-            foreach (var str in split)
-            {
-                if (!string.IsNullOrWhiteSpace(str))
-                    tokens.Add(str);
-            }
+		public void NextLine(string line)
+		{
+			if (string.IsNullOrWhiteSpace(line))
+				return;
 
-            if (tokens.Count > 0)
-            {
-                string name = tokens[0];
-                List<string> param = tokens.GetRange(1, tokens.Count - 1);
-                IObjCommand cmd = null;
-                if(commands.TryGetValue(name, out cmd))
-                {
-                    cmd.Execute(param, this);
-                }
-            }            
-        }
-    }
+			var split = line.Split(" ");
+			List<string> tokens = new List<string>();
+			foreach (var str in split)
+			{
+				if (!string.IsNullOrWhiteSpace(str))
+					tokens.Add(str);
+			}
+
+			if (tokens.Count > 0)
+			{
+				string name = tokens[0];
+				List<string> param = tokens.GetRange(1, tokens.Count - 1);
+				IObjCommand cmd = null;
+				if (commands.TryGetValue(name, out cmd))
+				{
+					cmd.Execute(param, this);
+				}
+			}
+		}
+	}
 }
-   
+
