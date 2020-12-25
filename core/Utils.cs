fcabb58ca5d5638e6eaa7e3fdc58853d92a73fe1
yishenhan <yishenhan@tencent.com>
2020-12-25 23:08:35 +0800

    update utils

--- core/Utils.cs
+++ core/Utils.cs
@@ -6,11 +6,36 @@ namespace WavefrontObjSharp
 {
 	public static class Utils
 	{
+		public static Dictionary<int, string> spaceStr = new Dictionary<int, string>();
+		public static string GetSpaceStr(int len)
+        {
+			string result = null;
+			if(spaceStr.TryGetValue(len, out result))
+            {
+				return result;
+            }
+			StringBuilder sb = new StringBuilder();
+			for(int i = 0; i < len; i++)
+            {
+				sb.Append(" ");
+            }
+			result = sb.ToString();
+			spaceStr[len] = result;
+			return result;
+        }
 		public static string Dump(this Vector vector)
 		{
 			if (vector == null)
 				return "<null>";
-			return string.Format("<{0}>", string.Join(", ", new List<float>(vector.v).ConvertAll(v => string.Format("{0,9}", v))));
+			return string.Format("< {0} >", string.Join(", ", new List<float>(vector.v).ConvertAll(
+                (v) => {
+					var str = string.Format("{0:####.000}", v);
+					int strlen = str.Length;
+					if (strlen < 8)
+						return GetSpaceStr(8 - strlen) + str;
+					else
+						return str;
+				})));
 		}
 
 		public static string Dump(List<Vector> vector, string indent = "")
@@ -35,13 +60,17 @@ namespace WavefrontObjSharp
 		{
 			StringBuilder sb = new StringBuilder();
 			int componentCount = mesh.componentCount;
+			if (selectNames == null)
+				selectNames = mesh.componentNames.ToArray();
 			for (int i = 0; i < componentCount; i++)
 			{
 				sb.Append(string.Format("[{0}]:\n{1}\n", mesh.componentNames[i], Dump(mesh.data[mesh.componentNames[i]], indent)));
 			}
 			var vertexArray = mesh.CreateVectorArrayList(selectNames);
+			sb.Append(string.Format("[va] ['{0}']\n", string.Join("','", selectNames)));
 			foreach(var v in vertexArray)
             {
+				sb.Append(indent);
 				foreach(var e in v)
                 {
 					sb.Append(e.Dump());
